9ca701d6dac4a8d90213884428463f8b6f9f5615
AhammedSajal <sajalmiracle7@gmail.com>
2025-06-03 13:18:26 +0100

    Initial project push

---
+++ lib/Helper/Color.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primary_app = MaterialColor(
+  0xff26897E,
+  <int, Color>{
+  50: primary,
+      100: primary,
+      200: primary,
+      300: primary,
+      400: primary,
+      500: primary,
+      600: primary,
+      700: primary,
+      800: primary,
+      900: primary,
+  },
+);
+const Color primary = Color(0xff26897E);
+const Color secondary = Color(0xff1EBAAA);
+const Color fontColor = Color(0xff1F1F1F);
+const Color disableColor = Color(0xffEEF2F9);
+const Color pink = Color(0xff1EBAAA);
+const Color red = Colors.red;
+const Color lightBlack = Color(0xff6D6D6D);
+const Color lightBlack2 = Color(0xff999999);
+const Color lightWhite = Color(0xffF5F5F5);
+const Color white = Color(0xffFFFFFF);
+const Color cardColor = Color(0xffFFFFFF);
---
+++ lib/Screens/Splash/Splash.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart';
+import '../../Helper/Color.dart';
+import '../../Helper/Constant.dart';
+import '../../Helper/Session.dart';
+import '../../Helper/String.dart';
+import '../Authentication/Login.dart';
+import '../Home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<Splash> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top],);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
+    getSettings();
+    super.initState();
+    startTime();
+  }
+
+  getSettings() async {
+    final Response response = await post(getSettingApi, headers: headers)
+        .timeout(const Duration(seconds: timeOut));
+    final data = json.decode(response.body)['data'];
+    final auth = data['authentication_settings'][0]['authentication_method'];
+    cityWiseDelivery =
+        data['system_settings'][0]['city_wise_deliverability'] == "1";
+    isFirebaseAuth = auth == "firebase";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+    body: SizedBox.expand(
+      child: Image.asset(
+        'assets/images/splash.png', // <-- Use your PNG path here
+        fit: BoxFit.cover,          // Fills the whole screen
+      ),
+    ),
+  );
+  }
+
+  Future<Timer> startTime() async {
+    const duration = Duration(seconds: 2);
+    return Timer(duration, navigationPage);
+  }
+
+  Future<void> navigationPage() async {
+    final bool isFirstTime = await getPrefrenceBool(isLogin);
+    if (isFirstTime) {
+      Navigator.pushReplacement(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => const Home(),
+        ),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => const Login(),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values,);
+    super.dispose();
+  }
+}
